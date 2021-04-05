aa71453253907224a777724d0744c45538c17505
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-05 11:37:59 +0200

    useless imports removed from tests files

--- app/components/__tests__/Main.js
+++ app/components/__tests__/Main.js
@@ -1,11 +1,6 @@
-import "react-native";
 import React from "react";
-
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configure({ adapter: new Adapter() });
 import { shallow } from "enzyme";
+
 import MainComponent from "../Main";
 
 it("render a MainComponent", function () {
