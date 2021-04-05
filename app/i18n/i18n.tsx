76c42abb201fb070885dcc8178212374724cec0d
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-05 12:00:58 +0200

    i18n config file moved to i18n folder

--- app/i18n/i18n.tsx
+++ app/i18n/i18n.tsx
@@ -1,6 +1,7 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import { config } from "../config/index";
+
+import { config } from "./config";
 import commonEn from "./common/en.json";
 import commonFr from "./common/fr.json";
 
