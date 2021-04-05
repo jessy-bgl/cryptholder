6fad39249259eb07fa4b695d2719c3eeab5c7443
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-05 11:55:15 +0200

    setupTests moved to Test folder and add jest react-i18next mock in order to avoid warnings

--- test/enzyme.js
+++ test/enzyme.js
@@ -1,4 +1,3 @@
-import "react-native";
 //import "jest-enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Enzyme from "enzyme";
---
+++ test/setup.ts
@@ -0,0 +1,8 @@
+// we always make sure 'react-native' gets included first
+import "react-native";
+
+// enzyme configuration
+import "./enzyme";
+
+// libraries to mock
+import "./mock-i18next";
