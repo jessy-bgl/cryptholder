239de6ca0524fd33c8e4e63ea9ccedf18f7b6019
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-05 11:06:21 +0200

    unused components removed

--- app/components/__tests__/Main.js
+++ app/components/__tests__/Main.js
@@ -1,24 +1,15 @@
-import 'react-native';
-import React from 'react';
+import "react-native";
+import React from "react";
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import '@testing-library/jest-dom/extend-expect'
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-// NOTE: test renderer must be required after react-native.
 Enzyme.configure({ adapter: new Adapter() });
-import { shallow } from 'enzyme';
-import MainComponent from '../Main';
-import { Text } from 'react-native'
+import { shallow } from "enzyme";
+import MainComponent from "../Main";
 
-it('render a MainComponent', function() {
-  const wrapper = shallow(
-    <MainComponent />
-  );
+it("render a MainComponent", function () {
+  const wrapper = shallow(<MainComponent />);
 
   expect(wrapper.exists()).toBe(true);
-
-  expect(wrapper.contains(
-    <Text>Home Screen</Text>
-  ))
 });
--- app/components/__tests__/Settings.js
+++ app/components/__tests__/Settings.js
@@ -1,24 +1,15 @@
-import 'react-native';
-import React from 'react';
+import "react-native";
+import React from "react";
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import '@testing-library/jest-dom/extend-expect'
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-// NOTE: test renderer must be required after react-native.
 Enzyme.configure({ adapter: new Adapter() });
-import { shallow } from 'enzyme';
-import SettingsComponent from '../Settings';
-import { Text } from 'react-native'
+import { shallow } from "enzyme";
+import SettingsComponent from "../Settings";
 
-it('render a SettingsComponent', function() {
-  const wrapper = shallow(
-    <SettingsComponent />
-  );
+it("render a SettingsComponent", function () {
+  const wrapper = shallow(<SettingsComponent />);
 
   expect(wrapper.exists()).toBe(true);
-
-  expect(wrapper.contains(
-    <Text>Settings Screen</Text>
-  ))
 });
