856becc206c658c460cadb2c9785910368be41c9
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-06 21:16:46 +0200

    fetch coins market data moved to main screen instead of component

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,26 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Text } from "react-native";
 import { useTranslation } from "react-i18next";
-import { observer } from "mobx-react-lite";
 
-import { useStore } from "../models/root-store/root-store";
-
-const Main = observer(() => {
+const Main = () => {
   const { t } = useTranslation("common");
-  const { coins } = useStore();
-
-  useEffect(() => {
-    async function fetchCoinsMarketsData() {
-      await coins.getMarketData();
-    }
-    fetchCoinsMarketsData();
-  }, []);
 
   return (
     <>
       <Text>{t("home")}</Text>
     </>
   );
-});
+};
 
 export default Main;
--- app/screens/Main.tsx
+++ app/screens/Main.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import { observer } from "mobx-react-lite";
+
+import { useStore } from "../models/root-store/root-store";
 import MainComponent from "../components/Main";
 
-export default function Main() {
+export const MainScreen = observer(function Main() {
+  const { coins } = useStore();
+  const { marketData } = coins;
+
+  useEffect(() => {
+    async function fetchCoinsMarketsData() {
+      await coins.getMarketData();
+    }
+    fetchCoinsMarketsData();
+  }, []);
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <MainComponent />
     </View>
   );
-}
+});
+
+export default MainScreen;
