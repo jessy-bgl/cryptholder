00ade3a2c3357b0d599c3a477d95e61c2035e090
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-06 22:14:38 +0200

    loading market data added to Main state

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Text } from "react-native";
 import { useTranslation } from "react-i18next";
+import { boolean } from "mobx-state-tree/dist/internal";
 
-const Main = () => {
+type MainProps = {
+  loadingData: boolean;
+};
+
+const Main = ({ loadingData }: MainProps) => {
   const { t } = useTranslation("common");
 
   return (
