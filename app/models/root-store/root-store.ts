f375d2ef6233d94ac72e5b15f6bdb6f18c8acf83
Jessy Bogalho <j.bogalho@m2mfactory.fr>
2021-04-07 22:50:53 +0200

    setup root store moved into a function

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -11,12 +11,6 @@ const RootStoreModel = types.model({
   coins: types.optional(Coins, {} as any),
 });
 
-let initialState = RootStoreModel.create({
-  coins: {},
-});
-
-export const rootStore = initialState;
-
 // ------------------------------------------------------------------ //
 // all the code above corresponds to the recommended way of using
 // MST with React : https://mobx-state-tree.js.org/concepts/using-react
@@ -25,7 +19,7 @@ export const rootStore = initialState;
 /**
  * The RootStore instance.
  */
-export type RootInstance = Instance<typeof RootStoreModel>;
+export type RootStoreInstance = Instance<typeof RootStoreModel>;
 
 /**
  * Create a context we can use to
@@ -33,12 +27,14 @@ export type RootInstance = Instance<typeof RootStoreModel>;
  * - Consume stores in our screens (or other components, though it's
  *   preferable to just connect screens)
  */
-const RootStoreContext = createContext<RootInstance>({} as RootInstance);
+const RootStoreContext = createContext<RootStoreInstance>(
+  {} as RootStoreInstance
+);
 
 /**
  * The provider our root component will use to expose the root store
  */
-export const Provider = RootStoreContext.Provider;
+export const RootStoreProvider = RootStoreContext.Provider;
 
 /**
  * A hook that screens can use to gain access to our stores,
@@ -46,3 +42,16 @@ export const Provider = RootStoreContext.Provider;
  * or less likely: `const rootStore = useStore()`
  */
 export const useStore = () => useContext(RootStoreContext);
+
+/**
+ * Setup the root state
+ */
+export const setupRootStore = async () => {
+  let rootStore: RootStoreInstance;
+
+  rootStore = RootStoreModel.create({
+    coins: {},
+  });
+
+  return rootStore;
+};
