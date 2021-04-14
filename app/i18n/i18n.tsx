0f8d19021939e93ff82b801ad4de063221057590
Louis Aussedat <aussedat.louis@gmail.com>
2021-04-14 21:10:57 +0200

    i18n: add settings files

--- app/i18n/i18n.tsx
+++ app/i18n/i18n.tsx
@@ -4,6 +4,8 @@ import { initReactI18next } from "react-i18next";
 import { config } from "./config";
 import commonEn from "./common/en.json";
 import commonFr from "./common/fr.json";
+import settingsEn from "./settings/en.json";
+import settingsFr from "./settings/fr.json";
 
 export const initializeI18n = async (): Promise<void> => {
   await i18next.use(initReactI18next).init({
@@ -12,9 +14,11 @@ export const initializeI18n = async (): Promise<void> => {
     resources: {
       en: {
         common: commonEn,
+        settings: settingsEn,
       },
       fr: {
         common: commonFr,
+        settings: settingsFr,
       },
     },
   });
