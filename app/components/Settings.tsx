5897734fe885cb8c05328375e5f79444570c6766
Louis Aussedat <aussedat.louis@gmail.com>
2021-04-16 22:59:26 +0200

    Settings: update component with ItemIonicon

--- app/components/Settings.tsx
+++ app/components/Settings.tsx
@@ -2,17 +2,9 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import { List, Switch, Text } from "react-native-paper";
 import { useTranslation } from "react-i18next";
-import { Ionicons } from "@expo/vector-icons";
 import ListItemDivider from "./List/ListItemDivider";
 import { useStore } from "../models/root-store/root-store-context";
-
-type ItemIconProps = {
-  name: React.ComponentProps<typeof Ionicons>["name"];
-};
-
-const ItemIcon = ({ name }: ItemIconProps) => (
-  <Ionicons size={20} name={name} style={styles.icon} />
-);
+import ItemIonicon from "../components/Icon/ItemIonicon";
 
 export default function Settings() {
   const { t } = useTranslation("settings");
@@ -22,12 +14,12 @@ export default function Settings() {
     <List.Section style={styles.root}>
       <ListItemDivider
         title={t("language")}
-        left={() => <ItemIcon name={"globe-outline"} />}
+        left={() => <ItemIonicon name={"globe-outline"} />}
         right={() => <Text style={styles.text}>FR</Text>}
       />
       <ListItemDivider
         title={t("theme")}
-        left={() => <ItemIcon name={"contrast-outline"} />}
+        left={() => <ItemIonicon name={"contrast-outline"} />}
         right={() => (
           <Switch
             value={settings.darkMode}
@@ -37,28 +29,28 @@ export default function Settings() {
       />
       <ListItemDivider
         title={t("mainCurrency")}
-        left={() => <ItemIcon name={"cash-outline"} />}
+        left={() => <ItemIonicon name={"cash-outline"} />}
         right={() => <Text style={styles.text}>USD</Text>}
       />
       <ListItemDivider
         title={t("portfolioOptions")}
-        left={() => <ItemIcon name={"settings-outline"} />}
-        right={() => <ItemIcon name={"chevron-forward-outline"} />}
+        left={() => <ItemIonicon name={"settings-outline"} />}
+        right={() => <ItemIonicon name={"chevron-forward-outline"} />}
       />
       <ListItemDivider
         title={t("homeScreen")}
-        left={() => <ItemIcon name={"albums-outline"} />}
+        left={() => <ItemIonicon name={"albums-outline"} />}
         right={() => <Text style={styles.text}>Accueil</Text>}
       />
       <ListItemDivider
         title={t("security")}
-        left={() => <ItemIcon name={"lock-closed-outline"} />}
-        right={() => <ItemIcon name={"chevron-forward-outline"} />}
+        left={() => <ItemIonicon name={"lock-closed-outline"} />}
+        right={() => <ItemIonicon name={"chevron-forward-outline"} />}
       />
       <ListItemDivider
         title={t("about")}
-        left={() => <ItemIcon name={"rocket-outline"} />}
-        right={() => <ItemIcon name={"chevron-forward-outline"} />}
+        left={() => <ItemIonicon name={"rocket-outline"} />}
+        right={() => <ItemIonicon name={"chevron-forward-outline"} />}
       />
     </List.Section>
   );
