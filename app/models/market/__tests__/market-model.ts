8c244f3112400bb9a2dab1874df7c1917c110bd0
Jessy <jessy.bgl95@gmail.com>
2021-04-19 20:57:35 +0200

    market model test moved

--- app/models/market/__tests__/market-model.ts
+++ app/models/market/__tests__/market-model.ts
@@ -1,6 +1,6 @@
 import { MarketModel } from "../market-model";
 
-describe("Market store", () => {
+describe("Market model", () => {
   test("can be created", () => {
     const instance = MarketModel.create({});
     expect(instance).toBeTruthy();
