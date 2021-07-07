5238d2d62c6906510502a1452080329fbcbfedbb
Jessy <jessy.bogalho@gmail.com>
2021-07-07 22:52:25 +0200

    minor refactoring

--- app/screens/Favorites.tsx
+++ app/screens/Favorites.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { View } from "react-native";
-import FavoritesComponent from "../components/Favorites"
+import { StyleSheet, View, StatusBar } from "react-native";
+
+import FavoritesComponent from "../components/Favorites";
 
 export default function Favorites() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.root}>
       <FavoritesComponent />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+});
