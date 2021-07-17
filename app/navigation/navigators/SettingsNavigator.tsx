6da0ea3b854fb0fd59b6eb1b7b2bf3d820f364c2
Louis Aussedat <aussedat.louis@gmail.com>
2021-07-17 09:18:16 +0200

    SettingsNavigator: minor refactoring

--- app/navigation/navigators/SettingsNavigator.tsx
+++ app/navigation/navigators/SettingsNavigator.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useTranslation } from "react-i18next";
+import { useTheme } from "react-native-paper";
 
 import Settings from "../../screens/settings/Settings";
 import SettingsLanguage from "../../screens/settings/SettingsLanguage";
 import SettingsMainCurrency from "../../screens/settings/SettingsMainCurrency";
 import SettingsMainScreen from "../../screens/settings/SettingsMainScreen";
 import SettingsAbout from "../../screens/settings/SettingsAbout";
-import { useTheme } from "react-native-paper";
 
 export type SettingsStackParamList = {
   settings: undefined;
@@ -22,6 +22,12 @@ const Stack = createStackNavigator<SettingsStackParamList>();
 const SettingsNavigator = () => {
   const { t } = useTranslation("settings");
   const { colors } = useTheme();
+  const headerStyle = {
+    headerStyle: {
+      backgroundColor: colors.secondary,
+    },
+    headerTintColor: colors.text,
+  };
 
   return (
     <Stack.Navigator initialRouteName="settings">
@@ -30,10 +36,7 @@ const SettingsNavigator = () => {
         component={Settings}
         options={{
           title: t("settings"),
-          headerStyle: {
-            backgroundColor: colors.secondary,
-          },
-          headerTintColor: colors.text,
+          ...headerStyle,
         }}
       />
       <Stack.Screen
@@ -41,10 +44,7 @@ const SettingsNavigator = () => {
         component={SettingsLanguage}
         options={{
           title: t("language"),
-          headerStyle: {
-            backgroundColor: colors.secondary,
-          },
-          headerTintColor: colors.text,
+          ...headerStyle,
         }}
       />
       <Stack.Screen
@@ -52,10 +52,7 @@ const SettingsNavigator = () => {
         component={SettingsMainCurrency}
         options={{
           title: t("mainCurrency"),
-          headerStyle: {
-            backgroundColor: colors.secondary,
-          },
-          headerTintColor: colors.text,
+          ...headerStyle,
         }}
       />
       <Stack.Screen
@@ -63,10 +60,7 @@ const SettingsNavigator = () => {
         component={SettingsMainScreen}
         options={{
           title: t("homeScreen"),
-          headerStyle: {
-            backgroundColor: colors.secondary,
-          },
-          headerTintColor: colors.text,
+          ...headerStyle,
         }}
       />
       <Stack.Screen
@@ -74,10 +68,7 @@ const SettingsNavigator = () => {
         component={SettingsAbout}
         options={{
           title: t("about"),
-          headerStyle: {
-            backgroundColor: colors.secondary,
-          },
-          headerTintColor: colors.text,
+          ...headerStyle,
         }}
       />
     </Stack.Navigator>
